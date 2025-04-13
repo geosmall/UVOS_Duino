1bbb4dd3807d7db073d1cff130d41975ef68303e
George Small <geomall92@gmail.com>
2025-04-13 19:55:23 -0400

    Added Arduino Stream, Print, iota, wiring

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "wiring.h"
+
+/* sketch */
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+// Weak empty variant initialization function.
+// May be redefined by variant files.
+// extern void initVariant() __attribute__((weak));
+
+extern void setup(void);
+extern void loop(void);
+
+// void yield(void);
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
